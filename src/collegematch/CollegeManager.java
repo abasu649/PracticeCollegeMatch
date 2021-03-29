4f33e93f8e83dbd10f3e8da6f5446f2dbf8c96ec
abasu649 <anamika.basu@wustl.edu>
2021-03-28 18:58:48 -0700

    started working

---
+++ src/collegematch/CollegeManager.java
@@ -0,0 +1,34 @@
+package collegematch;
+
+import java.io.*; 
+
+import java.util.Scanner;  
+
+import java.util.ArrayList;
+
+public class CollegeManager {
+	private ArrayList<College> colleges;
+	
+	public CollegeManager() {
+		this.colleges = new ArrayList<College>();
+		College washU = new College("Washington University", 1470, 3.5, "urban");
+		College harvard = new College("Harvard College", 1600, 3.9, "urban");
+		College dartmouth = new College("Dartmouth College", 1490, 3.8, "rural");
+		College iowaState = new College("Iowa State University", 1400, 3.2, "rural");
+		colleges.add(washU);
+		colleges.add(harvard);
+		colleges.add(dartmouth);
+		colleges.add(iowaState);
+	}
+	
+	public void searchCollege(String collegeName) {
+		for (College college : colleges) {
+			if (college.getName() == collegeName) {
+				college.displayCollegeInformation();
+			}
+		}
+		System.out.println("College not found.");
+	}
+
+
+}
---
+++ src/collegematch/UserManager.java
@@ -0,0 +1,65 @@
+package collegematch;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class UserManager {
+	
+	private ArrayList<User> users;
+	private Scanner keyboardIn;
+	
+	public UserManager() {
+		this.users = new ArrayList<User>();
+	}
+	
+	public User logIn(String userName) {
+		for (User user: users) {
+			if (user.getUsername().equals(userName)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public void register(String userName, int satScore, double gpa, String campusPreference) {
+		User user = new User(userName, satScore, gpa, campusPreference);
+		users.add(user);
+	}
+	
+	public boolean validateCampusPreference(String campusPreference) {
+		if (campusPreference.equals("rural" )|| campusPreference.equals("urban")) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkForDuplicateUsername(String userName) {
+		for (User user: users) {
+			if (user.getUsername() == userName) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean validateSATScore(int satScore) {
+		if (satScore >= 400 && satScore <= 1600) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean validateGPA(double gpa) {
+		if (gpa >= 0.0 && gpa <= 4.0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void displayUserSavedCollegeList(User user) {
+		user.displaySavedColleges();
+	}
+
+	
+}
